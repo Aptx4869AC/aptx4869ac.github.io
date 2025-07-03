19487a38c8e75d605269b87e03529cfd9747601e
Jh Li <115479290+Aptx4869AC@users.noreply.github.com>
2025-07-03 19:01:59 +0800

    refactor：优化了界面

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1751481476181";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1751540375555";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
