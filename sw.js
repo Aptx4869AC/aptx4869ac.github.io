96a74e8b897cd080e8ad74e7cfb6ef9b3cc2f008
Jh Li <115479290+Aptx4869AC@users.noreply.github.com>
2025-07-04 02:44:20 +0800

    feat：修改图标

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1751551904064";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1751568154346";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
